30c4a49e5b93ad0581e1fbabc1b88a16a7b89ddd
Victor Cunha <99662236+victor-cunha@users.noreply.github.com>
2022-02-17 16:23:11 -0300

    Add files via upload

---
+++ Extremamente Básico.cpp
@@ -0,0 +1,21 @@
+ #include<stdio.h>
+ 
+int main(){
+
+	int valorA, valorB, valorX;
+	
+	printf("Informe o valor de A\n:");
+	scanf("%2d", &valorA );
+	printf("Informe o valor de B\n:");
+	scanf("%2d", &valorB);
+	    
+		valorX = valorA + valorB;
+		
+	printf("o resultado da adicao entre valor A e valor B , foi de: %d, valorX");
+	
+					 
+	
+	
+	
+	return 0;
+}
