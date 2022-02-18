2c1c84b0a475c0007b4a63c2861b21f961ff5cdd
Victor Cunha <99662236+victor-cunha@users.noreply.github.com>
2022-02-18 10:44:05 -0300

    Add files via upload

---
+++ intervalo.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+    
+	int main(){
+		double numero;
+		printf("incluir o numero:" );
+		scanf("%f" , &numero );
+		 {
+		 if(numero >= && numero <= 25);
+		 printf("intervalo [0,25]");
+		 else if(numero > 25 && numero <=50);
+		 printf("intervalo [25,50]");
+		 else if(numero > 50 && numero <=75);
+		 printf("intervalo [50,75]");
+		 else if(numero > 75 && numero <=100);
+	     printf("intervalo [75,100]");
+	     else printf("fora do intervalo");
+	
+	     return 0;
+	
+	
+}
